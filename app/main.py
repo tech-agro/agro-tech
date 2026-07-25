db5ad418f5ec366551690252602511a076bfcc45
luis-fil <luis.fbferreira@ufape.edu.br>
2026-07-25 17:36:40 -0300

    feat: include producao router in main application

--- app/main.py
+++ app/main.py
@@ -7,10 +7,12 @@ from fastapi import FastAPI
 from app.core.database import check_connection
 from app.identity.controller import router as identity_router
 from app.identity.controller import users_router
+from app.producao.controller import router as producao_router
 
 app = FastAPI(title="Agro Tech API")
 app.include_router(identity_router)
 app.include_router(users_router)
+app.include_router(producao_router)
 
 
 @app.get("/health")
